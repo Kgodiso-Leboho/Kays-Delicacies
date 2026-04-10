b7825e41761a7022bbeb403460e327332d895601
Kgodiso-Leboho <kgodisoaustinleboho@gmail.com>
2026-04-10 07:08:59 +0200

    Setup tailwindcss

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold text-blue-500">
+      Tailwind v4 is working 🚀
+    </h1>
+  );
+}
