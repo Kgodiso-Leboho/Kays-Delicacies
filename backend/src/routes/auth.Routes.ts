3c8ba9a97da78cd43f419ec6077fa8a0dff051c2
Kgodiso-Leboho <kgodisoaustinleboho@gmail.com>
2026-05-13 11:53:48 +0200

    Feat: Added the verify email route

--- backend/src/routes/auth.Routes.ts
+++ backend/src/routes/auth.Routes.ts
@@ -1,14 +1,18 @@
 import express from 'express';
-import { getDataOfLoggedInUser, loginUser, logoutUser, registerUser } from '../controllers/auth.Controller.js';
+import { getDataOfLoggedInUser, loginUser, logoutUser, registerUser, verifyEmail } from '../controllers/auth.Controller.js';
 import { authMiddleware } from '../middleware/auth.middleware.js';
 const router = express.Router();
 import { apiLimiter } from '../utils/rateLimiter.js';
+import { AuthRequest } from '../types/express.js';
 
 // Apply rate limiter to all auth routes
 router.use(apiLimiter);
 
 // Register a new user
 router.post('/register', registerUser);
+
+// Verify a new user's email
+router.get('/verify-email', verifyEmail);
 
 // User login
 router.post('/login', loginUser);
